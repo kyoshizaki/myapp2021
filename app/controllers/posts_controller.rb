505a330b4653d0bfc64569f4a5ef3d717253d8ba
Koichi Yoshizaki <kyoshi@oita-u.ac.jp>
2021-06-18 11:32:46 +0000

    Lesson09の課題

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,24 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end
 
+    def create
+        # render plain: "受信パラメター：#{params}"
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to posts_path
+        else
+            render "new"
+        end
+    end
+
     def new
+        @post = Post.new
+    end
+
+private
+
+    def post_params
+        params.require(:post).permit(:title, :body)
     end
 
 end
